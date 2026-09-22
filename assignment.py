a3396a385f022d3c25e8acb3f27e4043897c4120
Soyombo <soyombo.b2015@orkhonschool.edu.mn>
2026-09-22 13:14:38 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,8 +2,17 @@
 # Exercise 1
 
 def find_closest(data, target):
-    # Write your code here
-    pass
+    low = 0
+    high = len(data) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if data[mid] == target:
+            return mid  # found
+        elif data[mid] < target:
+            low = mid + 1  # target is in the right half
+        else:
+            high = mid - 1  # target is in the left half
+    return data[mid]
 
 # Exercise 2
 def integer_sqrt(n):
